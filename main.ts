39eb1f2f3dce05c0714f277497bb02b0555d1d7c
andersonaguilar601-netizen <andersonaguilar601@gmail.com>
2025-10-07 10:58:54 -0600

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,148 @@
+input.onButtonPressed(Button.A, function () {
+    basic.clearScreen()
+    basic.pause(2000)
+    basic.showString("te parece si jugamos?  ")
+    basic.showIcon(IconNames.Happy)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Happy)
+    basic.showLeds(`
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        . . . . .
+        `)
+    basic.showIcon(IconNames.Happy)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    # # # # #
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . # # # .
+    . # # # .
+    . # # # .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    # . . . #
+    . # . # .
+    . . # . .
+    . # . # .
+    # . . . #
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . # . # .
+    . . # . .
+    . # . # .
+    . . . . .
+    `)
+basic.pause(100)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . # . .
+    . . . . .
+    . . . . .
+    `)
+basic.showLeds(`
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    . . . . .
+    `)
+basic.pause(1000)
+basic.clearScreen()
+basic.showString("Hola grupo")
+basic.showIcon(IconNames.Happy)
+basic.pause(500)
+basic.showIcon(IconNames.Heart)
+basic.pause(200)
+basic.showIcon(IconNames.SmallHeart)
+basic.pause(200)
+basic.showIcon(IconNames.Heart)
+basic.pause(200)
+basic.showIcon(IconNames.SmallHeart)
+basic.pause(200)
+basic.showIcon(IconNames.Heart)
+basic.pause(200)
+basic.showIcon(IconNames.SmallHeart)
+basic.pause(200)
+basic.showIcon(IconNames.Heart)
+basic.pause(200)
+basic.showIcon(IconNames.SmallHeart)
+basic.pause(200)
+basic.showIcon(IconNames.Heart)
